d1d4ff07b409ba86a2a70817c0a417a6404f0c6c
KatliamSec <135433207+KatliamSec@users.noreply.github.com>
2023-06-03 16:23:22 +0400

    index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked by KatliamSec</title>
+</head>
+<body style="background-color: black;">
+    <div align="center">
+        <br><br>
+        <a href="https://illegalplatform.com">
+            <img src="https://2.bp.blogspot.com/-N1vRuccf_Uk/XG1txTsZqUI/AAAAAAAAA9g/lcbUEQxTaCQKVPhOT51n1ZvC5P3QObMyQCLcBGAs/s1600/narcos1.gif" alt="Hacked by KatliamSec"/>
+            <br><br>
+        </a>
+        </font>
+        <br><br>
+        <font face="Courier New" color="white" size="6">
+            <span style="text-shadow: 0px 0px 9px ;">Hacked by</span>
+        </font>
+        <font face="Courier New" color="red" size="6">
+            <span style="text-shadow: 0px 0px 9px ;">KatliamSec</span>
+        </font>
+        <br><br>
+        <font face="Courier New" color="white" size="5">
+            <span style="text-shadow: 0px 0px 9px ;">bu kadar deface konunuz var daha xssi kapatamamissiniz ;D</span>
+        </font>
+        <br><br>
+        <font face="Courier New" color="white" size="6">
+            <span style="text-shadow: 0px 0px 9px ;"><strong>instagram: @katliamsec</strong></span>
+        </font>
+    </div>
+</body>
+</html>
